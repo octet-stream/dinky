24e7eaee36acbf1145dc0965b9e2036a1707921d
octet-stream <nick.kruchinin@gmail.com>
2020-02-25 04:34:16 +0300

    Add Dinky#{tags,comments}() methods. Remove endpoint from Lists class. The List class is no more extends Requests and becomes a bunch of shortcuts for Search class.

--- test/unit/Lists.js
+++ test/unit/Lists.js
@@ -7,27 +7,6 @@ const createNoopLink = require("../helper/createNoopLink")
 
 test.beforeEach(createNoopLink)
 
-test("Creates request handler to the /lists.json path", async t => {
-  const link = t.context.noopLink
-
-  await new Lists({link}).last("4w")
-
-  const [[actual]] = link.firstCall.args
-
-  t.is(actual, "lists")
-})
-
-test(".last() sets the \"last\" parameter with given period", async t => {
-  const link = t.context.noopLink
-
-  await new Lists({link}).last("4w")
-
-  const [, query] = link.firstCall.args
-
-  t.is(query.get("last"), "4w")
-})
-
-
 test(
   ".topScoring() appends parameters to search for top scoring images",
   async t => {
@@ -35,7 +14,7 @@ test(
 
     const dinky = pq("../../lib/Dinky", {"./util/link": () => link})()
 
-    await new Lists({link, dinky}).topScoring()
+    await new Lists({dinky}).topScoring()
 
     const [, query] = link.firstCall.args
 
@@ -52,7 +31,7 @@ test(
 
     const dinky = pq("../../lib/Dinky", {"./util/link": () => link})()
 
-    await new Lists({link, dinky}).topScoringAllTime()
+    await new Lists({dinky}).topScoringAllTime()
 
     const [, query] = link.firstCall.args
 
@@ -69,7 +48,7 @@ test(
 
     const dinky = pq("../../lib/Dinky", {"./util/link": () => link})()
 
-    await new Lists({link, dinky}).topCommented()
+    await new Lists({dinky}).topCommented()
 
     const [, query] = link.firstCall.args
 
