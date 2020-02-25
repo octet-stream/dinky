a0dbab9f9c95fa4aa1f1778b194d0cd5aae4bc0d
octet-stream <nick.kruchinin@gmail.com>
2020-02-25 04:40:48 +0300

    Increase Dinky class coverage

--- test/unit/Dinky.js
+++ test/unit/Dinky.js
@@ -3,18 +3,27 @@ const pq = require("proxyquire")
 
 const {spy} = require("sinon")
 
-const dinky = require("../../lib/Dinky")
-// const Lists = require("../../lib/Lists")
-const Images = require("../../lib/Images")
+const Comments = require("../../lib/Comments")
 const Search = require("../../lib/Search")
+const Images = require("../../lib/Images")
+const dinky = require("../../lib/Dinky")
+const Lists = require("../../lib/Lists")
+const Tags = require("../../lib/Tags")
 
-// TODO: see lib/Dinky.js:63
-// test(".lists() returns the Lists instance", t => {
-//   t.true(dinky().lists() instanceof Lists)
-// })
+test(".lists() returns the Lists instance", t => {
+  t.true(dinky().lists() instanceof Lists)
+})
 
 test(".images() returns the Images instance", t => {
   t.true(dinky().images() instanceof Images)
+})
+
+test(".tags() returns the Tags instance", t => {
+  t.true(dinky().tags() instanceof Tags)
+})
+
+test(".comments() returns the Comments instance", t => {
+  t.true(dinky().comments() instanceof Comments)
 })
 
 test(".search() returns the Search instance", t => {
