f4fe314676472951a324d8a055c860be1d4ff282
Nick K <nick.kruchinin@gmail.com>
2021-03-15 05:15:01 +0300

    Add tests for User class

--- src/User.ts
+++ src/User.ts
@@ -2,7 +2,7 @@ import r from "./type/responses"
 
 import {Entities, EntitiesOptions} from "./Entities";
 
-class User extends Entities<r.User, r.UserResponse> {
+export class User extends Entities<r.User, r.UserResponse> {
   constructor(options?: EntitiesOptions) {
     super({...options, path: "profiles"})
   }
