90c648a4d7aac7bde798243dd98c5c06b8585946
Nick K <nick.kruchinin@gmail.com>
2021-03-26 17:48:58 +0300

    Rename: User -> Profiles

--- lib/Profiles.test.ts
+++ lib/Profiles.test.ts
@@ -3,12 +3,12 @@ import test from "ava"
 import createNoopLink from "./__helper__/createNoopLink"
 
 import {Link} from "./util/link"
-import {User} from "./User"
+import {Profiles} from "./Profiles"
 
 test("Creates a new request to /api/v1/json/profiles", async t => {
   const link = createNoopLink<[[string]]>()
 
-  await new User({link: link as any as Link})
+  await new Profiles({link: link as any as Link})
 
   const [[path]] = link.firstCall.args as [[string]]
 
--- lib/Profiles.ts
+++ lib/Profiles.ts
@@ -2,10 +2,8 @@ import r from "./type/responses"
 
 import {Entities, EntitiesOptions} from "./Entities"
 
-export class User extends Entities<r.User, r.UserResponse> {
+export class Profiles extends Entities<r.User, r.UserResponse> {
   constructor(options?: EntitiesOptions) {
     super({...options, path: "profiles"})
   }
 }
-
-export default User
