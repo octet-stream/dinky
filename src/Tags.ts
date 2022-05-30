4ccbd4b8c1eeff98f148569a22a98e695a455e11
Nick K <nick.kruchinin@gmail.com>
2022-05-31 01:53:51 +0300

    Small fix

--- src/Tags.ts
+++ src/Tags.ts
@@ -3,7 +3,7 @@ import r from "./responses.js"
 import {Entities} from "./Entities.js"
 import type {EntitiesOptions} from "./Entities.js"
 import {instaniate} from "./util/instaniate.js"
-import {LinkOptions} from "util/link.js"
+import {LinkOptions} from "./util/link.js"
 
 export class Tags extends Entities<r.Tag, r.TagsResponse> {
   constructor(options?: EntitiesOptions) {
