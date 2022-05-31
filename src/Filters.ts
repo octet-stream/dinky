910dcd9c5c26ffd258fac83a5339da9521c4a3f8
Nick K <nick.kruchinin@gmail.com>
2022-05-31 03:45:01 +0300

    Add return type for Filters.user() method.

--- src/Filters.ts
+++ src/Filters.ts
@@ -14,7 +14,7 @@ export class Filters extends Entities<r.Filter, r.FiltersResponse> {
     return this._link([...this._path, "system"], this._query, options)
   }
 
-  user(options?: LinkOptions) {
+  user(options?: LinkOptions): Promise<r.FiltersResponse> {
     return this._link([...this._path, "user"], this._query, options)
   }
 }
