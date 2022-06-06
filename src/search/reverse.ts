2353d98fbbee9d40748bfff2a71fecb575cfe046
Nick K <nick.kruchinin@gmail.com>
2022-06-06 05:33:00 +0300

    Add example for search.reverse

--- src/search/reverse.ts
+++ src/search/reverse.ts
@@ -8,6 +8,14 @@ import {SearchImages} from "./SearchImages.js"
  *
  * @param url The URL of image
  * @param options Request options
+ *
+ * @example
+ *
+ * ```js
+ * import {search} from "dinky.js"
+ *
+ * await search.reverse("https://derpicdn.net/img/2019/12/24/2228439/full.jpg")
+ * ```
  */
 export const reverse = (url: string, options?: LinkOptions) => {
   const search = new SearchImages()
