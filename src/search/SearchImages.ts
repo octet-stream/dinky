fd5f593fa68a563ccc2f3c306774d9c714b3b77b
Nick K <nick.kruchinin@gmail.com>
2022-06-06 22:10:17 +0300

    Fix a typo

--- src/search/SearchImages.ts
+++ src/search/SearchImages.ts
@@ -161,7 +161,7 @@ export class SearchImages extends Search<"images"> {
    * ```
    */
   reverse(url: string, options?: LinkOptions) {
-    // Override search type to "reserve" so request will be send to the right search endpoing
+    // Override search type to "reserve" so request will be send to the right search endpoint
     this._type = "reverse"
 
     this._query.set("url", url)
